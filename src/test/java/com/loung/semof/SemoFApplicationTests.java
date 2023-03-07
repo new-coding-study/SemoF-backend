5a1ecf45a4a7c3a489b80e33e784d847d25eaeb1
MSI <ppp@pp.pp>
2023-03-07 18:14:19 +0900

    test

---
+++ src/test/java/com/loung/semof/SemoFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loung.semof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoFApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
