748b3966c78e1e37e3683e8685914052337d6d2b
sik <seonsik7@gmail.com>
2023-03-21 15:42:02 +0900

    - 근태 관련 파일 생성 진행 (수정 필요)

---
+++ src/main/java/com/loung/semof/attendance/controller/AttendanceController.java
@@ -0,0 +1,136 @@
+package com.loung.semof.attendance.controller;
+
+import com.loung.semof.common.ResponseDto;
+import com.loung.semof.common.paging.Pagenation;
+import com.loung.semof.common.paging.ResponseDtoWithPaging;
+import com.loung.semof.common.paging.SelectCriteria;
+import com.loung.semof.attendance.dto.AttendanceDto;
+import com.loung.semof.attendance.service.AttendanceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
+public class AttendanceController {
+
+
+    private final AttendanceService attendanceService;
+
+    public AttendanceController(AttendanceService attendanceService) {
+        this.attendanceService = attendanceService;
+    }
+
+    /* 총 갯수 구해서 페이징 처리한 스티커 전체 조회 */
+    @GetMapping("/attendances")
+    public ResponseEntity<ResponseDto> selectAttendanceListWithPaging(@RequestParam(name="offset", defaultValue="1") String offset) {
+
+        log.info("[AttendanceController] selectAttendanceListWithPaging : " + offset);
+
+        int totalCount = attendanceService.selectAttendanceTotal();   //총 갯수 구하기
+        int limit = 10;
+        int buttonAmount = 5;
+
+        SelectCriteria selectCriteria = Pagenation.getSelectCriteria(Integer.parseInt(offset), totalCount, limit, buttonAmount);
+
+        log.info("[AttendanceController] selectCriteria : " + selectCriteria);
+
+        ResponseDtoWithPaging responseDtoWithPaging = new ResponseDtoWithPaging();
+        responseDtoWithPaging.setPageInfo(selectCriteria);
+        responseDtoWithPaging.setData(attendanceService.selectAttendanceListWithPaging(selectCriteria));
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "전체 조회 성공", responseDtoWithPaging));
+    }
+
+    /* 스티커 상세 조회 */
+    @GetMapping("/attendances/{attendanceCode}")
+    public ResponseEntity<ResponseDto> selectAttendanceDetail(@PathVariable String attendanceCode) {
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "스티커 상세정보 조회 성공",  attendanceService.selectAttendance(attendanceCode)));
+    }
+
+    /* 카테고리별 리스트 조회 */
+    @GetMapping("/attendances/categories/{categoryCode}")
+    public ResponseEntity<ResponseDto> selectAttendanceListAboutCategory(@PathVariable String categoryCode) {
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "카테고리별 스티커 조회 성공",  attendanceService.selectAttendanceListAboutCategory(categoryCode)));
+    }
+
+    /* 타입별 리스트 조회 */
+    @GetMapping("/attendances/types/{typeCode}")
+    public ResponseEntity<ResponseDto> selectAttendanceListAboutType(@PathVariable String typeCode) {
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "타입별 스티커 조회 성공",  attendanceService.selectAttendanceListAboutType(typeCode)));
+    }
+
+    /* 스티커 전체조회 (관리자) */
+    @GetMapping("/attendances-management")
+    public ResponseEntity<ResponseDto> selectAttendanceListWithPagingForAdmin(@RequestParam(name="offset", defaultValue="1") String offset) {
+
+        log.info("[AttendanceController] selectAttendanceListWithPagingForAdmin : " + offset);
+
+        int totalCount = attendanceService.selectAttendanceTotal();
+        int limit = 10;
+        int buttonAmount = 5;
+        SelectCriteria selectCriteria = Pagenation.getSelectCriteria(Integer.parseInt(offset), totalCount, limit, buttonAmount);;
+
+        log.info("[AttendanceController] selectCriteria : " + selectCriteria);
+
+        ResponseDtoWithPaging responseDtoWithPaging = new ResponseDtoWithPaging();
+        responseDtoWithPaging.setPageInfo(selectCriteria);
+        responseDtoWithPaging.setData(attendanceService.selectAttendanceListWithPaging(selectCriteria));
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "조회 성공", responseDtoWithPaging));
+    }
+
+    /* 스티커 상세조회 (관리자) */
+    @GetMapping("/attendances-management/{attendanceCode}")
+    public ResponseEntity<ResponseDto> selectAttendanceDetailForAdmin(@PathVariable String attendanceCode) {
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "상품 상세정보 조회 성공", attendanceService.selectAttendance(attendanceCode)));
+    }
+
+    /* 스티커 등록 (관리자) */
+    @PostMapping(value = "/attendances-management")
+    public ResponseEntity<ResponseDto> insertAttendance(@ModelAttribute AttendanceDto attendanceDto) {
+        log.info("[AttendanceController] PostMapping attendanceDto : " + attendanceDto);
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "스티커 등록 성공",  attendanceService.insertAttendance(attendanceDto)));
+    }
+
+    /* 스티커 수정 (관리자) */
+    @PutMapping(value = "/attendances-management/{attendanceNo}")
+    public ResponseEntity<ResponseDto> updateAttendance(@ModelAttribute AttendanceDto attendanceDto, @PathVariable String attendanceNo) {
+        log.info("[AttendanceController]PutMapping attendanceNo : " + attendanceNo);
+        log.info("[AttendanceController]PutMapping attendanceDto : " + attendanceDto);
+
+        attendanceDto.setAttendanceCode(Integer.parseInt(attendanceNo));
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "스티커 수정 성공",  attendanceService.updateAttendance(attendanceDto)));
+    }
+
+    /* 스티커 삭제 */
+    @DeleteMapping("/attendances-management/{attendanceNo}")
+    public ResponseEntity<ResponseDto> deleteAttendance(@PathVariable String attendanceNo) {
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "스티커 삭제 성공",  attendanceService.deleteAttendance(attendanceNo)));
+    }
+
+    /* 스티커 검색 */
+    @GetMapping("/attendances/search")
+    public ResponseEntity<ResponseDto> selectSearchList(@RequestParam(name="condition", defaultValue = "attendanceName") String condition, @RequestParam(name="searchValue", required = false) String searchValue ) {
+
+        HashMap<String, String> search = new HashMap<>();
+        search.put("condition", condition);
+        search.put("searchValue", searchValue );
+
+        System.out.println(condition);
+        System.out.println(searchValue);
+
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "조회 성공",  attendanceService.selectSearchAttendanceList(search)));
+    }
+
+}
---
+++ src/main/java/com/loung/semof/attendance/service/AttendanceService.java
@@ -0,0 +1,193 @@
+package com.loung.semof.attendance.service;
+
+import com.loung.semof.common.paging.SelectCriteria;
+import com.loung.semof.attendance.dao.AttendanceMapper;
+import com.loung.semof.attendance.dto.ReadAttendanceDto;
+import com.loung.semof.attendance.dto.AttendanceDto;
+import com.loung.semof.util.FileUploadUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+@Slf4j
+@Service
+public class AttendanceService {
+
+    @Value("${image.image-dir}")
+    private String IMAGE_DIR;
+    @Value("${image.image-url}")
+    private String IMAGE_URL;
+
+    private final AttendanceMapper attendanceMapper;
+
+    public AttendanceService(AttendanceMapper attendanceMapper) {
+        this.attendanceMapper = attendanceMapper;
+    }
+
+    /* 총 갯수 구하기 */
+    public int selectAttendanceTotal() {
+        log.info("[AttendanceService] selectAttendanceTotal Start ===================================");
+        int result = attendanceMapper.selectAttendanceTotal();
+
+        log.info("[AttendanceService] selectAttendanceTotal End ===================================");
+        return result;
+    }
+
+    /* 페이징 처리 된 전체 조회 */
+    public Object selectAttendanceListWithPaging(SelectCriteria selectCriteria) {
+        log.info("[AttendanceService] selectAttendanceListWithPaging Start ===================================");
+        List<ReadAttendanceDto> attendanceList = attendanceMapper.selectAttendanceListWithPaging(selectCriteria);
+
+        for(int i = 0 ; i < attendanceList.size() ; i++) {
+            attendanceList.get(i).setAttendanceImageUrl(IMAGE_URL + attendanceList.get(i).getAttendanceImageUrl());
+        }
+        log.info("[AttendanceService] selectAttendanceListWithPaging End ===================================");
+        return attendanceList;
+    }
+
+    /* 스티커 상세 조회 */
+    public ReadAttendanceDto selectAttendance(String attendanceCode) {
+        log.info("[AttendanceService] selectAttendance Start ===================================");
+        ReadAttendanceDto attendanceDto = attendanceMapper.selectAttendance(attendanceCode);
+        attendanceDto.setAttendanceImageUrl(IMAGE_URL + attendanceDto.getAttendanceImageUrl());
+        log.info("[AttendanceService] selectAttendance End ===================================");
+        return attendanceDto;
+    }
+
+    /* 카테고리별 전체 조회 */
+    public List<ReadAttendanceDto> selectAttendanceListAboutCategory(String categoryCode) {
+        log.info("[AttendanceService] selectAttendanceListAboutCategory Start ===================================");
+
+        List<ReadAttendanceDto> selectAttendanceListAboutCategory = attendanceMapper.selectAttendanceListAboutCategory(categoryCode);
+
+        for(int i = 0 ; i < selectAttendanceListAboutCategory.size() ; i++) {
+            selectAttendanceListAboutCategory.get(i).setAttendanceImageUrl(IMAGE_URL + selectAttendanceListAboutCategory.get(i).getAttendanceImageUrl());
+        }
+
+        log.info("[AttendanceService] selectAttendanceListAboutCategory End ==============================");
+
+        return selectAttendanceListAboutCategory;
+    }
+
+    /* 타입별 전체 조회 */
+    public List<ReadAttendanceDto> selectAttendanceListAboutType(String typeCode) {
+        log.info("[AttendanceService] selectAttendanceListAboutType Start ===================================");
+
+        List<ReadAttendanceDto> selectAttendanceListAboutType = attendanceMapper.selectAttendanceListAboutType(typeCode);
+
+        for(int i = 0 ; i < selectAttendanceListAboutType.size() ; i++) {
+            selectAttendanceListAboutType.get(i).setAttendanceImageUrl(IMAGE_URL + selectAttendanceListAboutType.get(i).getAttendanceImageUrl());
+        }
+
+        log.info("[AttendanceService] selectAttendanceListAboutType End ==============================");
+
+        return selectAttendanceListAboutType;
+    }
+
+    /* 스티커 추가 */
+    @Transactional
+    public String insertAttendance(AttendanceDto attendanceDto) {
+        log.info("[AttendanceService] insertAttendance Start ===================================");
+        log.info("[AttendanceService] attendanceDto : " + attendanceDto);
+        String imageName = UUID.randomUUID().toString().replace("-", "");
+        String replaceFileName = null;
+        int result;
+        log.info("[AttendanceService] IMAGE_DIR : " + IMAGE_DIR);
+        log.info("[AttendanceService] imageName : " + imageName);
+        try {
+            replaceFileName = FileUploadUtils.saveFile(IMAGE_DIR, imageName, attendanceDto.getAttendanceImage());
+            log.info("[AttendanceService] replaceFileName : " + replaceFileName);
+
+            attendanceDto.setAttendanceImageUrl(replaceFileName);
+
+            log.info("[AttendanceService] insert Image Name : "+ replaceFileName);
+
+            result = attendanceMapper.insertAttendance(attendanceDto);
+        } catch (IOException e) {
+            log.info("[AttendanceService] IOException IMAGE_DIR : "+ IMAGE_DIR);
+
+            log.info("[AttendanceService] IOException deleteFile : "+ replaceFileName);
+
+            FileUploadUtils.deleteFile(IMAGE_DIR, replaceFileName);
+            throw new RuntimeException(e);
+        }
+        log.info("[AttendanceService] result > 0 성공: "+ result);
+        return (result > 0) ? "스티커 등록 성공" : "스티커 등록 실패";
+    }
+
+    /* 스티커 수정 */
+    @Transactional
+    public Object updateAttendance(AttendanceDto attendanceDto) {
+        log.info("[AttendanceService] updateAttendance Start ===================================");
+        log.info("[AttendanceService] attendanceDto : " + attendanceDto);
+        String replaceFileName = null;
+        int result;
+        try {
+            String oriImage = attendanceMapper.selectAttendance(String.valueOf(attendanceDto.getAttendanceCode())).getAttendanceImageUrl();
+            log.info("[updateAttendance] oriImage : " + oriImage);
+
+            if(attendanceDto.getAttendanceImage() != null){
+                // 이미지 변경 진행
+                String imageName = UUID.randomUUID().toString().replace("-", "");
+                replaceFileName = FileUploadUtils.saveFile(IMAGE_DIR, imageName, attendanceDto.getAttendanceImage());
+
+                log.info("[updateAttendance] IMAGE_DIR!!"+ IMAGE_DIR);
+                log.info("[updateAttendance] imageName!!"+ imageName);
+
+                log.info("[updateAttendance] InsertFileName : " + replaceFileName);
+                attendanceDto.setAttendanceImageUrl(replaceFileName);
+
+                log.info("[updateAttendance] deleteImage : " + oriImage);
+                boolean isDelete = FileUploadUtils.deleteFile(IMAGE_DIR, oriImage);
+                log.info("[update] isDelete : " + isDelete);
+            } else {
+                // 이미지 변경 없을 시
+                attendanceDto.setAttendanceImageUrl(oriImage);
+            }
+            result = attendanceMapper.updateAttendance(attendanceDto);
+        } catch (IOException e) {
+            log.info("[updateAttendance] Exception!!");
+            FileUploadUtils.deleteFile(IMAGE_DIR, replaceFileName);
+            throw new RuntimeException(e);
+        }
+        log.info("[AttendanceService] updateAttendance End ===================================");
+        log.info("[AttendanceService] result > 0 성공: "+ result);
+
+        return (result > 0) ? "스티커 업데이트 성공" : "스티커 업데이트 실패";
+    }
+
+    /* 스티커 삭제 */
+    @Transactional
+    public String deleteAttendance(String attendanceNo) {
+        log.info("[AttendanceService] deleteAttendance Start ===================================");
+        log.info("[AttendanceService] attendanceNo : " + attendanceNo);
+
+        int result;
+        result = attendanceMapper.deleteAttendance(attendanceNo);
+
+        log.info("[AttendanceService] result > 0 성공: "+ result);
+        return (result > 0) ? "스티커 삭제 성공" : "스티커 삭제 실패";
+    }
+
+    /* 스티커 검색 조회 */
+    public List<ReadAttendanceDto> selectSearchAttendanceList(HashMap<String, String> search) {
+        log.info("[AttendanceService] selectSearchAttendanceList Start ===================================");
+        log.info("[AttendanceService] searchValue : " + search);
+
+        List<ReadAttendanceDto> attendanceListWithSearchValue = attendanceMapper.attendanceListWithSearchValue(search);
+        log.info("[AttendanceService] attendanceListWithSearchValue : " + attendanceListWithSearchValue);
+
+        for(int i = 0 ; i < attendanceListWithSearchValue.size() ; i++) {
+            attendanceListWithSearchValue.get(i).setAttendanceImageUrl(IMAGE_URL + attendanceListWithSearchValue.get(i).getAttendanceImageUrl());
+        }
+        log.info("[AttendanceService] selectSearchAttendanceList End ===================================");
+
+        return attendanceListWithSearchValue;
+    }
+
+}
