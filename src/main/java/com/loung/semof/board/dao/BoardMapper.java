718d12baeb04564a66d8d85a8f368a3a6809095e
aiden <lih9490@gmail.com>
2023-03-22 20:35:10 +0900

    23.03.22 1차 commit

---
+++ src/main/java/com/loung/semof/board/dao/BoardMapper.java
@@ -0,0 +1,14 @@
+package com.loung.semof.board.dao;
+
+import com.loung.semof.board.dto.BoardDto;
+import com.loung.semof.common.paging.SelectCriteria;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    int selectNoticeTotal();
+
+    List<BoardDto> selectNoticeListWithPaging(SelectCriteria selectCriteria);
+}
