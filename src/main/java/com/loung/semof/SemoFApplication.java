c22870f8997b275a0a84e638ee55bba2350c376b
Anthony <113497486+AzuraLight@users.noreply.github.com>
2023-03-27 09:59:00 +0900

    Add files via upload

---
+++ src/main/java/com/loung/semof/SemoFApplication.java
@@ -0,0 +1,13 @@
+package com.loung.semof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemoFApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/loung/semof/member/dao/MemberMapper.java
@@ -0,0 +1,20 @@
+package com.loung.semof.member.dao;
+
+
+import com.loung.semof.member.dto.MemberDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+
+@Mapper
+public interface MemberMapper {
+
+    MemberDto selectByEmail(String email);
+
+    int insertMember(MemberDto member);
+
+    Optional<MemberDto> findByMemberId(String memberId);
+
+    MemberDto selectByMemberId(String memberId);
+}
