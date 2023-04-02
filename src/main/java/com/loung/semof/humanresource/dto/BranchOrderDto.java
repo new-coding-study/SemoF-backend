4694a43a0faf6b9f5ecf62e4f303817f5f5befc0
AzuraLight <113497486+AzuraLight@users.noreply.github.com>
2023-04-02 15:42:52 +0900

    23.04.02 15:42 HD - 소실 파일 복구

--- src/main/java/com/loung/semof/humanresource/dto/BranchOrderDto.java
+++ src/main/java/com/loung/semof/humanresource/dto/BranchOrderDto.java
@@ -1,30 +1,31 @@
 package com.loung.semof.humanresource.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.loung.semof.common.dto.BranchDto;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 /**
- * @파일이름 : EmployeePhotoDto.java
+ * @파일이름 : BranchOrderDto.java
  * @프로젝트 : SemoF
  * @버전관리 : 1.0.0
- * @작성일 : 2023-03-26
+ * @작성일 : 2023-03-31
  * @작성자 : 이현도
  * @클래스설명 : 모델(Model)과 뷰(View) 사이에서 데이터 전송을 담당하는 클래스
- *  */
-@Builder
+ */
 @ToString
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-public class EmployeePhotoDto {
-    private Long photoNo;   // 사진번호
-    private Long empNo; // 사원번호
-    private String filePath;    //증명사진경로
-    private String originName;  //증명사진원본명
-    private String changeName;  //증명사진수정명
+@AllArgsConstructor
+public class BranchOrderDto {
+
+    private Long branchOrderNo; //지점 발령번호
+    private Long empNo; //사원번호
+    private Long branchCode;    //지점코드
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
-    private LocalDateTime uploadDate;   //증명사진업로드일
+    private LocalDateTime orderDate;    //발령일
+    private BranchDto branchDto;
+    private Long newBCode;  // 새로운 지점 코드
 }
