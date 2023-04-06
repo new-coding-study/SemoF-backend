cfde3bf44ca51b230ba5eec1db9afd90b9456a84
sik <seonsik7@gmail.com>
2023-04-06 18:44:29 +0900

    2023-04-06(목) / 근태 관리 출퇴근 기능 완료, 코드 정리 - 출퇴근 기능 완료 (기록조회 예정)
    - 실패 TDD 보류

--- src/test/java/com/loung/semof/attendance/dao/AttendanceMapperTest.java
+++ src/test/java/com/loung/semof/attendance/dao/AttendanceMapperTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -33,6 +34,7 @@ class AttendanceMapperTest {
     @Test
     void 근태정보_조회_성공() throws Exception {
         //given
+        // FORNT에서 줄 값
 
         //when
         AttendanceDto attendanceDto = attendanceMapper.selectAttendanceDetail(1);
@@ -61,30 +63,22 @@ class AttendanceMapperTest {
     }
 
     @Test
-    void 연차_현황_조회_성공() throws Exception {
-        //given
-
-        //when
-        AttendanceDto attendanceDto = attendanceMapper.selectVacationDetail(1);
-
-        //then
-        System.out.println(attendanceDto);  //로그포제이 안 쓰고 그냥 출력문으로 확인
-        assertNotNull(attendanceDto);
-    }
-
-    @Test
     void 사원_근태_상태_변경_성공() throws Exception {
         //given
-        int empNo = 3;
+        int empNo = 21;
+        HashMap<String, String> data = new HashMap<>();
+        data.put("empNo", String.valueOf(empNo));
 
         //when
         AttendanceDto lastDate = attendanceMapper.selectAttendanceDetail(empNo);
         LocalDate currentDate = LocalDate.now();
+        // 금일 근무기록이 없거나 마지막 근무일이 금일과 다를경우 현재일 기준의 행 추가
         if (lastDate.getAtdDate() == null || !Objects.equals(currentDate.toString(), lastDate.getAtdDate().substring(0, 10))){
             //lastDate == null 이였는데(행 자체가 null), 쿼리를 사원정보는 나오게 바꿔놔서 null비교문도 행 자체가 아닌 호출한 날짜로 비교)
             System.out.println("-------------날짜 비교 if문 진입-------------");
             attendanceMapper.insertAttendance(empNo);
         }
+        // 현재일 기준으로 근무상태기록 행 카운트
         int countAtt = attendanceMapper.selectCountAttendanceStatusInfo(empNo);
         int statusCode;
         // 상태값과 카운트된 기록에 따라 처리
@@ -102,14 +96,13 @@ class AttendanceMapperTest {
                 throw new Exception("에러 발생 (오늘 근무가 종료되었거나 잘못된 입력값");
         }
         int atdNo = attendanceMapper.selectLastAttendanceNo(empNo);
-        // HashMap<String, Integer> atdObject = new HashMap<>();
-        // atdObject.put("atdNo", atdNo);
-        // atdObject.put("statusCode", statusCode);
-        // atdObject.put("empNo", empNo);
-        attendanceMapper.insertAttendanceStatusInfo(atdNo, statusCode);
+        data.put("atdNo", String.valueOf(atdNo));
+        data.put("statusCode", String.valueOf(statusCode));
+        attendanceMapper.insertAttendanceStatusInfo(data);
         int result = attendanceMapper.updateAttendance(atdNo, empNo, statusCode);
 
         //then
+        System.out.println("data" + data);
         System.out.println("lastDate" + lastDate);
         System.out.println("currentDate" + currentDate);
         System.out.println("selectCountAttendanceStatusInfo : " + attendanceMapper.selectCountAttendanceStatusInfo(empNo));
@@ -133,25 +126,4 @@ class AttendanceMapperTest {
         System.out.println(currentDate);
         assertTrue(tot);
     }
-
-
-
-    /* @Test
-    @DisplayName("신규 메뉴 추가 확인")
-    public void 신규_메뉴_추가용_매퍼_테스트() {
-
-        //given
-        MenuDTO menu = new MenuDTO();
-        menu.setName("JUnitTestMenu");
-        menu.setPrice(10000);
-        menu.setCategoryCode(1);
-        menu.setOrderableStatus("Y");
-
-        //when
-        int result = menuMapper.registMenu(menu);
-
-        //then
-        assertEquals(1, result);
-    } */
-
 }
